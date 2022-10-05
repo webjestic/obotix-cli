a79ee2abe89a518df40d4c2bd662b1601c360666
Michael N <37873018+webjestic@users.noreply.github.com>
2022-10-05 09:17:12 -0400

    i5 | updated package and removed debug (#6)
    Co-authored-by: Michael Newlon <michael.newlon@tn.gov>

--- bin/obotix/cmd.js
+++ bin/obotix/cmd.js
@@ -2,13 +2,11 @@
 
 const backend = require( './backend' )
 
-
 exports.command = 'create <service> [folder]'
 exports.describe = 'Create a new mytn backend service'
 
 exports.handler = function (argv) {
-    // console.log(argv)
-    // console.log(argv._[0])
+
     if (argv.service === 'formstack') {
         console.log('creating formstack')
         formstack.create(argv)
