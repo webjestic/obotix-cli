eb3fc22410e928ec0132db4519c1a3c24c3b0bea
Michael Newlon <michael.newlon@tn.gov>
2022-10-05 09:16:39 -0400

    i5 | updated package and removed debug

--- bin/index.js
+++ bin/index.js
@@ -1,7 +1,5 @@
 
 
-const debug = require('debug')('app:index')
-
 var yargs = require('yargs')
 yargs.command(require('./obotix/cmd'))
   .help()
--- bin/obotix/copyRepo.js
+++ bin/obotix/copyRepo.js
@@ -1,33 +1,23 @@
 
 
-const debug = require('debug')('app:formstack')
 const conf = require('config')
 const fs = require('fs')
 const shell = require('shelljs')
 
 
 exports.copy = function (argv) {
-    console.log(argv)
-    console.log(argv._[0])
 
     const serviceRepo = `${argv.service}.repo`
 
-    debug('argv %O', argv)
     const localDir = process.cwd()
     const targetDir = localDir + "/" + argv.folder
     const oldGitDir = targetDir + '/.git'
-    debug('argv %O', argv) 
-    debug('pwd       :', localDir)
-    debug('targetDir :', targetDir)
-    debug('oldGitDir :', oldGitDir)
   
     // clone git repo into desired project name
     try {
         console.log(`Creating ${argv.folder}...`)
         let cmd = `git clone ${conf.get(serviceRepo)} ${argv.folder}`
-        debug('cmd', cmd)
         let result = shell.exec(cmd)
-        debug('result', result)
         if (result.code === 0) console.log(`${argv.folder} successfully created.`)
         fs.rmSync(oldGitDir, { recursive: true, force: true })
     } catch (error) {
