653cd5109310c4cb7d6eb383ee8474b7dec22ca0
Michael Newlon <michael.newlon@tn.gov>
2022-10-05 18:18:31 -0400

    i17 | readme details and TODO after folder create

--- README.md
+++ README.md
@@ -12,5 +12,24 @@ To upgrade the command line, run the following:
 To uninstall the command line, run the following:
 `npm uninstall -g obotix-cli`
 
-To use this
-`obotix create backend project-name`
+## Create a new project
+```bash
+# NEXT STEPS:
+#
+obotix-cli create backend project-name
+cd backend project-name
+echo NODE_ENV=devlopment > .env
+
+# NOTE: use with git version >= 2.28.0
+git init -b main 
+# NOTE: git version < 2.28.0
+git init && git symbolic-ref HEAD refs/heads/main 
+
+git add . && git commit -m "initial commit from CLI scaffold"
+git remote add origin <REMOTE_URL>
+git remote -v
+git push -u origin main
+```
+
+Location of globals
+`~/.nvm/versions/node/v16.15.1/bin`
--- bin/obotix/backend.js
+++ bin/obotix/backend.js
@@ -5,4 +5,24 @@ const repo = require('./copyRepo')
 exports.create = function (argv) {
     argv.gitrepo = "https://github.com/webjestic/obotix-mservice-scaffold.git"
     repo.copy(argv)
+
+    console.log('')
+    console.log('### NEXT STEPS: TODO')
+    console.log('')
+    console.log(`cd backend ${argv.folder}`)
+    console.log('echo NODE_ENV=devlopment > .env')
+    console.log('')
+    console.log('# NOTE: use with git version >= 2.28.0')
+    console.log('git init -b main')
+    console.log('# NOTE: git version < 2.28.0')
+    console.log('git init && git symbolic-ref HEAD refs/heads/main ')
+    console.log('')
+    console.log('git add . && git commit -m "initial commit from CLI scaffold"')
+    console.log('git remote add origin <REMOTE_URL>')
+    console.log('')
+    console.log('git remote -v')
+    console.log('git push -u origin main')
+    console.log('')
+
+
 }
